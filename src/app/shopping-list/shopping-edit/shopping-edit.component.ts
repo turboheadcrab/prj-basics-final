cf7747b00d6d0702229c24f50d2aae775d7ad241
Vladimir Apraksin <turbo.headcrab.666@gmail.com>
2023-11-06 16:35:51 -0500

    Allowing the User to Add Ingredients to the Shopping List

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import { Ingredient } from "../../shared/ingredient.model";
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit {
+export class ShoppingEditComponent {
 
-  constructor() { }
+  @ViewChild('nameInput')
+  nameInputRef: ElementRef;
 
-  ngOnInit() {
+  @ViewChild('amountInput')
+  amountInputRef: ElementRef;
+
+  @Output()
+  ingredientAdded = new EventEmitter<Ingredient>();
+
+  constructor() {
   }
 
+
+  onAdd() {
+    this.ingredientAdded.emit({
+      name: this.nameInputRef.nativeElement.value,
+      amount: this.amountInputRef.nativeElement.value,
+    })
+  }
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 import { Ingredient } from '../shared/ingredient.model';
 
@@ -7,15 +7,18 @@ import { Ingredient } from '../shared/ingredient.model';
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent {
+
   ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
   ];
 
-  constructor() { }
+  constructor() {
+  }
 
-  ngOnInit() {
+  onIngredientAdded(ingredient: Ingredient) {
+    this.ingredients.push(new Ingredient(ingredient.name, ingredient.amount));
   }
 
 }
